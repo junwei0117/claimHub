7ad9488a6e71b43f133edae186bf558cceb4c948
mushroom0117 <v860117@gmail.com>
2019-01-30 12:12:15 +0800

    Refactor the deployment code The new version of `evm-lite-lib` have removed the `EVMLC.compileContract`, and `Keystore.decrypt` renamed to` Keystore.decryptAccount`.

--- deploy/deployContract.js
+++ deploy/deployContract.js
@@ -1,6 +1,9 @@
 const { EVMLC } = require('evm-lite-lib');
 const { DataDirectory } = require('evm-lite-lib');
 
+const solc = require('solc');
+const fs = require('fs');
+
 // Default from address
 const from = '0X3CECAA0F2A6054022320A7C4C0C229A8D9062244';
 
@@ -16,28 +19,32 @@ const dataDirectory = new DataDirectory('/Users/junwei/.evmlc');
 const password = 'password';
 
 // Contract Object
+const rawContractName = 'claimHub';
 const contractPath = 'contract/claimhub.sol';
-const contractName = 'claimHub';
 
 // Get keystore object from the keystore directory
 // For the from address so we can decrypt and sign
 const accountDecrypt = async () => {
-  const account = await dataDirectory.keystore.decrypt(from, password);
+  const account = await dataDirectory.keystore.decryptAccount(from, password);
   return account;
 };
 
 // Generate contract object with ABI and data
-const generateContract = async () => {
-  const compiled = await evmlc.compileContract(contractName, contractPath);
-  const contract = await evmlc.loadContract(compiled.abi, {
-    data: compiled.bytecode,
+const loadContract = async () => {
+  const contractFile = fs.readFileSync(contractPath, 'utf8');
+  const contractName = `:${rawContractName}`;
+  const compiledOutput = solc.compile(contractFile, 1);
+  const data = compiledOutput.contracts[contractName].bytecode;
+  const ABI = JSON.parse(compiledOutput.contracts[contractName].interface);
+  const contract = await evmlc.loadContract(ABI, {
+    data,
   });
   return contract;
 };
 
 const deployContract = async () => {
   const account = await accountDecrypt();
-  const contract = await generateContract();
+  const contract = await loadContract();
   const response = await contract.deploy(account);
   return response;
 };
