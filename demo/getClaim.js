b4202b1dd5ee4358bc57b6e1d7be5bb26d1946cd
mushroom0117 <v860117@gmail.com>
2019-01-30 13:44:55 +0800

    Remove the signing object

--- demo/getClaim.js
+++ demo/getClaim.js
@@ -1,5 +1,9 @@
 const { EVMLC } = require('evm-lite-lib');
 const { DataDirectory } = require('evm-lite-lib');
+const { BN } = require('web3-utils');
+
+const solc = require('solc');
+const fs = require('fs');
 
 // Default from address
 const from = '0X3CECAA0F2A6054022320A7C4C0C229A8D9062244';
@@ -12,49 +16,47 @@ const evmlc = new EVMLC('210.240.162.43', 8080, {
 });
 
 // Keystore object
-const dataDirectory = new DataDirectory('/Users/junwei/.evmlc/');
+const dataDirectory = new DataDirectory('/Users/junwei/.evmlc');
 const password = 'password';
 
 // Contract Object
+const rawContractName = 'claimHub';
 const contractPath = 'contract/claimhub.sol';
-const contractName = 'claimHub';
-const contractAddress = '0x7ee1d718810015129cb09ccf0cc33753f04cf4ad';
-
-// Claim Object
-const claimOwner = 'Ender';
-const claimIndex = 0;
+const contractAddress = '0xda22aa6ee1a8cce03837600d8a235f5a099c3ee7';
 
 // Get keystore object from the keystore directory
 // For the from address so we can decrypt and sign
 const accountDecrypt = async () => {
-  const account = await dataDirectory.keystore.decrypt(from, password);
+  const account = await dataDirectory.keystore.decryptAccount(from, password);
   return account;
 };
 
 // Generate contract object with ABI and data
-const generateContract = async () => {
-  const compiled = evmlc.compileContract(contractName, contractPath);
-  const contract = await evmlc.loadContract(compiled.abi, {
-    data: compiled.bytecode,
+const loadContract = async () => {
+  const contractFile = fs.readFileSync(contractPath, 'utf8');
+  const contractName = `:${rawContractName}`;
+  const compiledOutput = solc.compile(contractFile, 1);
+  const data = compiledOutput.contracts[contractName].bytecode;
+  const ABI = JSON.parse(compiledOutput.contracts[contractName].interface);
+  const contract = await evmlc.loadContract(ABI, {
+    data,
     contractAddress,
   });
   return contract;
 };
 
-const getClaim = async (account, cfContract) => {
-  const transaction = await cfContract.methods.getClaim(claimOwner, claimIndex);
-  await transaction.sign(account);
-  await transaction.submit();
-  return transaction;
-};
-
-const demo = async () => {
+const getClaim = async () => {
   const account = await accountDecrypt();
-  const cfContract = await generateContract();
-  const response = await getClaim(account, cfContract);
-  return response;
+  const contract = await loadContract();
+
+  const claimOwner = 'Junwei';
+
+  const transaction = await contract.methods.getClaim(claimOwner);
+  await transaction.sign(account);
+  const claimContent = await transaction.call();
+  return claimContent;
 };
 
-demo()
+getClaim()
   .then(res => console.log(res))
   .catch(error => console.log(error));
