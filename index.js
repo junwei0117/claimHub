51e791beb691ce742ec07791ab002e54fcc92f0d
mushroom0117 <v860117@gmail.com>
2019-03-04 00:15:41 +0800

    Remove the .then(), use a function to show the demo

--- index.js
+++ index.js
@@ -7,27 +7,30 @@ const compile = require('./classes/compile');
 const ClaimHub = require('./classes/ClaimHub');
 
 let evmlc;
-let evmlcDirectory;
 let claimHub;
-let account;
 
 const initEVMConnection = async () => {
-  evmlc = new EVMLC('evm0.capjupiter.com', 8080, {
-    from: '0X3F9D41ECEA757FC4E2B44BE3B38A788DE2F11AD7',
-    gas: 100000000,
-    gasPrice: 0,
-  });
+  try {
+    evmlc = new EVMLC('evm0.capjupiter.com', 8080, {
+      from: '0X3F9D41ECEA757FC4E2B44BE3B38A788DE2F11AD7',
+      gas: 100000000,
+      gasPrice: 0,
+    });
+  } catch (error) {
+    console.log(`\x1b[31m${error}\x1b[0m\n`);
+  }
 };
 
 const assignEvmlcDirectory = () => {
-  evmlcDirectory = new DataDirectory(
+  const evmlcDirectory = new DataDirectory(
     path.join(os.homedir(), '.evmlc'),
   );
+  return evmlcDirectory;
 };
 
-const decryptAccount = async (password) => {
+const decryptAccount = async (password, evmlcDirectory) => {
   try {
-    account = await evmlcDirectory.keystore.decryptAccount(
+    const account = await evmlcDirectory.keystore.decryptAccount(
       evmlc.defaultFrom,
       password,
     );
@@ -49,7 +52,7 @@ const loadContract = async (contractName, filename) => {
   }
 };
 
-const genClaimHubClass = (contract) => {
+const genClaimHubClass = (contract, account) => {
   try {
     claimHub = new ClaimHub.ClaimHub(contract, account);
   } catch (error) {
@@ -84,69 +87,84 @@ const getClaim = async (claimOwner) => {
   }
 };
 
-initEVMConnection()
-  .then(() => console.log(
+const demo = async () => {
+  // Step1
+  initEVMConnection();
+  console.log(
     'Step 1 ) \n'
-    + 'At the very beginning, we specify the host of the EVM-Lite and our default account.\n',
-  ))
-  .then(() => {
-    assignEvmlcDirectory();
-  })
-  .then(() => console.log(
+    + 'At the very beginning, we specify the host of the EVM-Lite and our default account.\n'
+    + `EVM-Lite Address : ${evmlc.host}\n`
+    + `Default Account : ${evmlc.defaultFrom}\n`,
+  );
+
+  // Step2
+  const evmlcDirectory = await assignEvmlcDirectory();
+  console.log(
     'Step 2 ) \n'
     + 'The private keys reside directly on our own device, so we need to specify \n'
-    + 'the location in order to decrypt the account later.\n',
-  ))
-  .then(() => {
-    const password = 'password';
-    decryptAccount(password);
-  })
-  .then(() => console.log(
+    + 'the location in order to decrypt the account later.\n'
+    + `Default EVMLC Location : ${os.homedir()}/.evmlc\n`,
+  );
+
+  // Step3
+  const password = 'password';
+  const account = await decryptAccount(password, evmlcDirectory);
+  console.log(
     'Step 3 ) \n'
-    + 'Get account from keystore and decrypt the account.\n',
-  ))
-  .then(() => {
-    const contractName = 'claimHub';
-    const filename = 'claimHub';
-    loadContract(contractName, filename);
-  })
-  .then((contract) => {
-    genClaimHubClass(contract);
-  })
-  .then(() => console.log(
-    'Step 4 ) \n'
+    + 'Get account from keystore and decrypt the account.\n'
+    + 'Decrypt Account :',
+  );
+  console.log(account);
+
+  // Step 4
+  const contractName = 'claimHub';
+  const filename = 'claimHub';
+  const contract = await loadContract(contractName, filename);
+  await genClaimHubClass(contract, account);
+  console.log(
+    '\n'
+    + 'Step 4 ) \n'
     + 'Compiled the Claimhub SmartContract. \n'
     + 'The contract was written in the high-level Solidity language which compiles \n'
-    + 'down to EVM bytecode.\n',
-  ))
-  .then(() => {
-    deploySmartContract();
-  })
-  .then(responce => console.log(
-    'Step 5 ) \n'
-    + `Receipt : ${responce}\n`
-    + 'To deploy the SmartContract we created an EVM transaction \n'
-    + 'with a data field containing the bytecode. After going through consensus, the \n'
-    + 'transaction is applied on every node, so every participant will run a copy of \n'
-    + 'the same code with the same data.\n',
-  ))
-  .then(() => {
-    const claimOwner = 'Junwei';
-    const claimContent = 'SZ9BFDNKYEPJPFNTDZWLMAKNXBCDUFUIXUIKA9GRYPYTTNCNKEWBBVPJXMLD9QPOHRXHMPRKLSBGMIHRL';
-    setClaim(claimOwner, claimContent);
-  })
-  .then(responce => console.log(
-    'Step 6 ) \n'
-    + `Transaction Responce : ${responce}\n`
-    + 'We created an EVM transaction to call the setClaim method of the SmartContract. \n'
-    + 'This will create a combination of claims in the contract, \n'
-    + 'in this claim will explain the ownership of the claim and the corresponding content.\n',
-  ))
-  .then(() => {
-    const claimOwner = 'Junwei';
-    getClaim(claimOwner);
-  })
-  .then(responce => console.log(
-    'Step 7 ) \n'
-    + `Junwei's claim content : ${responce}`,
-  ));
+    + 'down to EVM bytecode.'
+    + 'Smart Contract Object : ',
+  );
+  console.log(contract);
+
+  // Step 5
+  const receipt = await deploySmartContract();
+  console.log(
+    '\n'
+  + 'Step 5 ) \n'
+  + 'To deploy the SmartContract we created an EVM transaction \n'
+  + 'with a data field containing the bytecode. After going through consensus, the \n'
+  + 'transaction is applied on every node, so every participant will run a copy of \n'
+  + 'the same code with the same data.\n'
+  + 'Receipt: ',
+  );
+  console.log(receipt.receipt);
+
+  // Step 6
+  const claimOwner = 'Junwei';
+  const claimContent = 'SZ9BFDNKYEPJPFNTDZWLMAKNXBCDUFUIXUIKA9GRYPYTTNCNKEWBBVPJXMLD9QPOHRXHMPRKLSBGMIHRL';
+  const setResponce = await setClaim(claimOwner, claimContent);
+  console.log(
+    '\n'
+  + 'Step 6 ) \n'
+  + 'We created an EVM transaction to call the setClaim method of the SmartContract. \n'
+  + 'This will create a combination of claims in the contract, \n'
+  + 'in this claim will explain the ownership of the claim and the corresponding content.\n'
+  + 'Transaction Responce : ',
+  );
+  console.log(setResponce);
+
+  // Step 7
+  const getResponce = await getClaim(claimOwner);
+  console.log(
+    '\n'
+  + 'Step 7 ) \n'
+  + `Junwei's claim content : ${getResponce}`,
+  );
+};
+
+demo();
